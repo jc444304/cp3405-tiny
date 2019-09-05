fb8d23d488b99aae5b2d53bf3d1f85bae48a9a59
Yvan Burrie <yvan.burrie@hotmail.com>
2019-09-06 01:27:30 +1000

    Add menu to header

--- head.php
+++ head.php
@@ -16,3 +16,21 @@
 </head>
 
 <body>
+
+<div class="container">
+    <div class="dropdown">
+        <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Menu
+            <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+            <li><a href="index.php">Home</a></li>
+            <li class="divider"></li>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="notifications.php">Notifications</a></li>
+            <li><a href="friends.php">Friends</a></li>
+            <li><a href="friends.php">Search</a></li>
+            <li class="divider"></li>
+            <li><a href="settings.php">Settings</a></li>
+            <li><a href="logout.php">Sign Out</a></li>
+        </ul>
+    </div>
+</div>
