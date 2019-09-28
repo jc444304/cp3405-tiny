4ed3be9b88e7d21f90d94fe701b72acc382df247
Yvan Burrie <yvan.burrie@hotmail.com>
2019-09-28 21:53:11 +1000

    Add search panel on landing page
    Only sample widgets are used for the search panel.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,53 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <h1 class="title mb-md-auto">
-                    {{ config('app.name') }}
-                </h1>
-                <p>
-                    Welcome to joblink!
-                </p>
+                <div class="container pt-5 text-center">
+                    <div class="row">
+                        <div class="col-md-9 mx-auto">
+                            <h1 class="display-4">Find Jobs</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-group">
+                    <div class="card text-left">
+                        <div class="card-header">
+                            <h4>Terms</h4>
+                        </div>
+                        <div class="card-body">
+                            <input type="text" class="form-control" placeholder="Title, occupation, keyword, etc">
+                        </div>
+                    </div>
+                    <div class="card text-center">
+                        <div class="card-header">
+                            <h4>Categories</h4>
+                        </div>
+                        <div class="card-body">
+                            <select class="custom-select" id="">
+                                <option>Software Development</option>
+                                <option>Data Mining</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="card text-right">
+                        <div class="card-header">
+                            <h4>Location</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="input-group">
+                                <select class="custom-select" id="">
+                                    <option selected>Australia</option>
+                                    <optgroup label="Queensland">
+                                        <option>Cairns</option>
+                                        <option>Townsville</option>
+                                    </optgroup>
+                                </select>
+                                <div class="input-group-append">
+                                    <button type="button" class="btn btn-outline-primary">Search</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <form class="form-inline my-2 my-lg-0" action="{{ route('search') }}" method="POST">
                     @csrf
                     <input class="form-control mr-sm-2" type="search" placeholder="Enter search term ..." aria-label="'Search" name="searchTerm">
