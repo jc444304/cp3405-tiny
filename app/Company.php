00c3cd5277bef1ab1847c2eea887fa5a5ab5e7d8
thomasleumann <thomas.leumann@my.jcu.edu.au>
2019-10-01 16:00:39 +1000

    add website to fillable in company model

--- app/Company.php
+++ app/Company.php
@@ -12,7 +12,7 @@ class Company extends Model
      * @var array
      */
     protected $fillable = [
-        'name','email','aboutus','address'
+        'name','email','aboutus','address','website'
     ];
 
     /**
