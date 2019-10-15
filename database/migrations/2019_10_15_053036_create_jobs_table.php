16b5b08c116a1ec0537f71e788341d263797b943
Yvan Burrie <yvan.burrie@hotmail.com>
2019-10-15 15:40:50 +1000

    Create jobs migration

---
+++ database/migrations/2019_10_15_053036_create_jobs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('description')->nullable();
+            $table->string('type');
+            $table->string('location');
+            $table->string('industry')->nullable();
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
