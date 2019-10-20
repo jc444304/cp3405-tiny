deae8e63f9df69da4bcbb09e2b439f56fc11c7a9
Nickolasjucker <nickolas.jucker@my.jcu.edu.au>
2019-10-21 08:30:21 +1000

    Add footer (fixed)

--- public/js/app.js
+++ public/js/app.js
@@ -49507,8 +49507,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\cp3405-tiny\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\cp3405-tiny\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/cp3405-tiny/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/cp3405-tiny/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
