d6b8691f1e9c19140aee9effddadc831c235b8db
IsabelleCarlsson <isabelle.carlsson@my.jcu.edu.au>
2019-10-31 11:03:54 +1000

    Fix about page text formatting

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,75 +4,89 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-9">
-                <div class="text-center">
-                    <div class="card mb-4">
-                       <div class="card-header"><h1>About JobLink</h1></div>
-                            <div class="card-body">
-                                <p>JCU Joblink is a child project from the team called TINY of design thinking where our goal is to create a hub where individuals of the fine institution of James Cook University can come to seek jobs in the IT related field
-                                    from employers that are seeking students from this establishment. The difference between JCU Joblink and any other job seeking websites is that we incorporate a trifecta system between students
-                                , staff and employers where it’s a lot more personal.</p>
+                <div class="card mb-4">
+                    <div class="card-header text-center"><h1>About JobLink</h1></div>
+                    <div class="card-body text-justify">
+                        <p>JCU Joblink is a child project from the team called TINY of design thinking where our goal is
+                            to create a hub where individuals of the fine institution of James Cook University can come
+                            to seek jobs in the IT related field
+                            from employers that are seeking students from this establishment. The difference between JCU
+                            Joblink and any other job seeking websites is that we incorporate a trifecta system between
+                            students
+                            , staff and employers where it’s a lot more personal.</p>
 
-                                <p>Where teachers can help endorse students making it a bit easier to gain access into the work field during or after their studies.</p>
+                        <p>Where teachers can help endorse students making it a bit easier to gain access into the work
+                            field during or after their studies.</p>
 
-                                <p>Employers can look to teachers to give recommendations of appropriate students that could be an employee the company is looking for or they can search through students that match their search queries.</p>
+                        <p>Employers can look to teachers to give recommendations of appropriate students that could be
+                            an employee the company is looking for or they can search through students that match their
+                            search queries.</p>
 
-                                <p>Students can search for jobs as one normally would on a job search website or contact staff for job recommendations that they deem would be a beneficial application for the student to try out.</p>
+                        <p>Students can search for jobs as one normally would on a job search website or contact staff
+                            for job recommendations that they deem would be a beneficial application for the student to
+                            try out.</p>
 
-                                <p>This system should help in getting a job as without help it can be very hard at times as your resume can be lost in the pile of other people’s resumes that employers usually get. It’ll also help employers by giving them a place that they can go to find qualified employees with the insurance of them studying or have studied a higher education level.
-                                    </p>
-                            <img class="jcu-trifecta-image" src="/images/trifecta-system.png" alt="JCU Trifecta System">
-                            <div class="creator-container">
-                                <div class="creator-container-header-text"><h3><b>Creators</b></h3>
+                        <p>This system should help in getting a job as without help it can be very hard at times as your
+                            resume can be lost in the pile of other people’s resumes that employers usually get. It’ll
+                            also help employers by giving them a place that they can go to find qualified employees with
+                            the insurance of them studying or have studied a higher education level.
+                        </p>
+                        <img class="jcu-trifecta-image" src="/images/trifecta-system.png" alt="JCU Trifecta System">
+                        <div class="creator-container">
+                            <div class="creator-container-header-text"><h3><b>Creators</b></h3>
+                            </div>
+                            <div class="creator-individual-container">
+                                <div class="creator-individual-image">
+                                    <img class="creator-image" src="/images/no-image-available.png" alt="Creator Image">
                                 </div>
-                                <div class="creator-individual-container">
-                                    <div class="creator-individual-image">
-                                        <img class="creator-image" src="/images/no-image-available.png" alt="Creator Image">
+                                <div class="creator-individual-wrapper">
+                                    <div class="creator-indvidual-header"><h5>Thomas Leumann</h5>
                                     </div>
-                                    <div class="creator-individual-wrapper">
-                                        <div class="creator-indvidual-header"><h5>Thomas Leumann</h5>
-                                        </div>
-                                        <div class="creator-individual-text">
-                                            <p><b>Bio:</b></p>
-                                            <p>Third year JCU IT student of class code CP3405. </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="creator-individual-container">
-                                    <div class="creator-individual-image">
-                                        <img class="creator-image" src="/images/no-image-available.png" alt="Creator Image">
-                                    </div>
-                                    <div class="creator-individual-wrapper">
-                                        <div class="creator-indvidual-header"><h5>Isabelle Carlsson</h5>
-                                        </div>
-                                        <div class="creator-individual-text">
-                                            <p><b>Bio:</b></p>
-                                            <p>Third year JCU IT student of class code CP3405. Born in Sweden but have lived in Australia since 2014. Realised in her last semester of IT that she wants to be a rockstar.</p>
-                                        </div>
+                                    <div class="creator-individual-text">
+                                        <p><b>Bio:</b></p>
+                                        <p>Third year JCU IT student of class code CP3405. </p>
                                     </div>
                                 </div>
-                                <div class="creator-individual-container">
-                                    <div class="creator-individual-image">
-                                        <img class="creator-image" src="/images/no-image-available.png" alt="Creator Image">
+                            </div>
+                            <div class="creator-individual-container">
+                                <div class="creator-individual-image">
+                                    <img class="creator-image" src="/images/no-image-available.png" alt="Creator Image">
+                                </div>
+                                <div class="creator-individual-wrapper">
+                                    <div class="creator-indvidual-header"><h5>Isabelle Carlsson</h5>
                                     </div>
-                                    <div class="creator-individual-wrapper">
-                                        <div class="creator-indvidual-header"><h5>Yvan Burrie</h5>
-                                        </div>
-                                        <div class="creator-individual-text">
-                                            <p><b>Bio:</b></p>
-                                            <p>Third year JCU IT student of class code CP3405. </p>
-                                        </div>
+                                    <div class="creator-individual-text">
+                                        <p><b>Bio:</b></p>
+                                        <p>Third year JCU IT student of class code CP3405. Born in Sweden but have lived
+                                            in Australia since 2014. Realised in her last semester of IT that she wants
+                                            to be a rockstar.</p>
                                     </div>
                                 </div>
-                                <div class="creator-individual-container">
-                                    <div class="creator-individual-image">
-                                        <img class="creator-image" src="/images/no-image-available.png" alt="Creator Image">
+                            </div>
+                            <div class="creator-individual-container">
+                                <div class="creator-individual-image">
+                                    <img class="creator-image" src="/images/no-image-available.png" alt="Creator Image">
+                                </div>
+                                <div class="creator-individual-wrapper">
+                                    <div class="creator-indvidual-header"><h5>Yvan Burrie</h5>
                                     </div>
-                                    <div class="creator-individual-wrapper">
-                                        <div class="creator-indvidual-header"><h5>Nickolas Jucker</h5></div>
-                                        <div class="creator-individual-text">
-                                            <p><b>Bio:</b></p>
-                                            <p>Third year JCU IT student of class code CP3405. Born and raised in Cairns, Australia with Swiss background. Married since 2012. Enjoys the little things in life and dislikes most people.</p>
-                                        </div>
+                                    <div class="creator-individual-text">
+                                        <p><b>Bio:</b></p>
+                                        <p>Third year JCU IT student of class code CP3405. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="creator-individual-container">
+                                <div class="creator-individual-image">
+                                    <img class="creator-image" src="/images/no-image-available.png" alt="Creator Image">
+                                </div>
+                                <div class="creator-individual-wrapper">
+                                    <div class="creator-indvidual-header"><h5>Nickolas Jucker</h5></div>
+                                    <div class="creator-individual-text">
+                                        <p><b>Bio:</b></p>
+                                        <p>Third year JCU IT student of class code CP3405. Born and raised in Cairns,
+                                            Australia with Swiss background. Married since 2012. Enjoys the little
+                                            things in life and dislikes most people.</p>
                                     </div>
                                 </div>
                             </div>
